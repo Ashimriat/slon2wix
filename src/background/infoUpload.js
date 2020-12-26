01c5844622d294e6dfd9dadedcce222560fc1399
Аксенов Ярослав Игоревич <Aksenov.Y.Ig@sberbank.ru>
2020-12-26 05:10:58 +0300

    Финальный код

--- src/background/infoUpload.js
+++ src/background/infoUpload.js
@@ -12,25 +12,16 @@ export const uploadInfo = async (tabId, fileData) => {
 	itemsData = fileData;
 	chrome.tabs.executeScript(USED_TAB_ID, { file: 'content.js' }, async () => {
 		for (let categoryInfo of itemsData) {
-			tmpItemIndex = 0;
-			if (tmpCategoryIndex < 14) {
-				log(`Категория по индексу ${tmpCategoryIndex} пропущена`);
-			} else {
-				const { itemType, name, items } = categoryInfo;
-				log(`Загрузка категории по индексу ${tmpCategoryIndex}`);
-				for (let item of items) {
-					log(`Загрузка вещи по индексу ${tmpItemIndex}`);
-					await new Promise(resolve => {
-						chrome.tabs.sendMessage(
-							USED_TAB_ID,
-							{ type: MESSAGES.UPLOAD_ITEM_INFO, itemInfo: { item, name, itemType } },
-							resolve
-						);
-					});
-					tmpItemIndex++;
-				}
+			const { itemType, name, items } = categoryInfo;
+			for (let item of items) {
+				await new Promise(resolve => {
+					chrome.tabs.sendMessage(
+						USED_TAB_ID,
+						{ type: MESSAGES.UPLOAD_ITEM_INFO, itemInfo: { item, name, itemType } },
+						resolve
+					);
+				});
 			}
-			tmpCategoryIndex++
 		}
 	});
 };
